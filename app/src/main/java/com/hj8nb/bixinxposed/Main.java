3121659adabb71eb72d103ae9e99186e1ef5fdad
曾丹青 <1119055131@qq.com>
2018-03-30 00:02:40 +0800

    修改

--- app/src/main/java/com/hj8nb/bixinxposed/Main.java
+++ app/src/main/java/com/hj8nb/bixinxposed/Main.java
@@ -3,6 +3,9 @@ package com.hj8nb.bixinxposed;
 import android.app.Application;
 import android.content.Context;
 
+
+import java.lang.reflect.Method;
+
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -10,7 +13,6 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import de.robv.android.xposed.callbacks.XCallback;
 
 
 /**
@@ -26,70 +28,79 @@ public class Main implements IXposedHookLoadPackage,IXposedHookInitPackageResour
                 ClassLoader cl=((Context)param.args[0]).getClassLoader();
                 Class<?> hookclass=null;
                 try {
-                    hookclass=cl.loadClass("com.bixin.live.data.bean.room.PrivateLimitBean");
+                    hookclass=cl.loadClass("com.bixin.live.presentation.ui.main.MainActivity");
+                    Method [] methods=hookclass.getMethods();
+                    for (Method g:methods){
+                        XposedBridge.log("===="+g.getName());
+                    }
+                    /*XposedHelpers.findAndHookMethod(hookclass, "getId", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                            param.setResult(null);
+                            XposedBridge.log("====getId===="+param.getResult());
+
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getCome", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(0);
+                            XposedBridge.log("====getCome===="+param.getResult());
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getBsid", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                            param.setResult(null);
+                            XposedBridge.log("====getBsid===="+param.getResult());
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getCopyright", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                            param.setResult(null);
+                            XposedBridge.log("====getCopyright===="+param.getResult());
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getMoney", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult("0");
+                            XposedBridge.log("====getMoney===="+param.getResult());
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getOnline", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult("1");
+                            XposedBridge.log("====getOnline===="+param.getResult());
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getPrerequisite", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult("0");
+                            XposedBridge.log("====getPrerequisite===="+param.getResult());
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getPtid", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                            param.setResult(null);
+                            XposedBridge.log("====getPtid===="+param.getResult());
+                        }
+                    });
+                    XposedHelpers.findAndHookMethod(hookclass, "getPtname", new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(null);
+                            XposedBridge.log("====getPtname===="+param.getResult());
+                        }
+                    });*/
                 }catch (Exception e){
                     XposedBridge.log("寻找com.bixin.live.data.bean.room报错");
                     return;
                 }
-                XposedBridge.log("寻找com.bixin.live.data.bean.room成功");
-                XposedHelpers.findAndHookMethod(hookclass, "getBsid", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult("");
-                        XposedBridge.log("修改返回值getBsid");
-                    }
-                });
-                XposedHelpers.findAndHookMethod(hookclass, "getCome", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult(0);
-                        XposedBridge.log("修改返回值getCome");
-                    }
-                });
-                XposedHelpers.findAndHookMethod(hookclass, "getId", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult(null);
-                        XposedBridge.log("修改返回值getId");
-                    }
-                });
-                XposedHelpers.findAndHookMethod(hookclass, "getMoney", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult("");
-                        XposedBridge.log("修改返回值getMoney");
-                    }
-                });
-                XposedHelpers.findAndHookMethod(hookclass, "getOnline", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult(1);
-                        XposedBridge.log("修改返回值");
-                    }
-                });
-                XposedHelpers.findAndHookMethod(hookclass, "getPrerequisite", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult(null);
-                        XposedBridge.log("修改返回值getPrerequisite");
-                    }
-                });
-                XposedHelpers.findAndHookMethod(hookclass, "getPtname", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult(null);
-                        XposedBridge.log("修改返回值getPtname");
-                    }
-                });
-                XposedHelpers.findAndHookMethod(hookclass, "getPtid", new XC_MethodHook() {
-                    @Override
-                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        param.setResult(null);
-                        XposedBridge.log("修改返回值getPtid");
-                    }
-                });
-
-
             }
         });
     }
