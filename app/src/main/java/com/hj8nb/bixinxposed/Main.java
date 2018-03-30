f5d0a43677d4e81d50829f2ea426ff22a38ac64f
zengdq <1119055131@qq.com>
2018-03-30 19:25:53 +0800

    比心破解模块

--- app/src/main/java/com/hj8nb/bixinxposed/Main.java
+++ app/src/main/java/com/hj8nb/bixinxposed/Main.java
@@ -4,14 +4,14 @@ import android.app.Application;
 import android.content.Context;
 
 
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
-import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 
@@ -19,7 +19,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
  * Created by ThundeRobot on 2018/2/24.
  */
 
-public class Main implements IXposedHookLoadPackage,IXposedHookInitPackageResources {
+public class Main implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
@@ -28,86 +28,42 @@ public class Main implements IXposedHookLoadPackage,IXposedHookInitPackageResour
                 ClassLoader cl=((Context)param.args[0]).getClassLoader();
                 Class<?> hookclass=null;
                 try {
-                    hookclass=cl.loadClass("com.bixin.live.presentation.ui.main.MainActivity");
-                    Method [] methods=hookclass.getMethods();
-                    for (Method g:methods){
-                        XposedBridge.log("===="+g.getName());
-                    }
-                    /*XposedHelpers.findAndHookMethod(hookclass, "getId", new XC_MethodHook() {
+                    hookclass=cl.loadClass("com.bixin.live.data.bean.BaseResponse");
+                    XposedHelpers.findAndHookMethod(hookclass, "getData", new XC_MethodHook() {
                         @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                            param.setResult(null);
-                            XposedBridge.log("====getId===="+param.getResult());
-
+                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                            try{
+                                String className=param.getResult().getClass().getName().toString();
+                                String patt = "PrivateLimitBean";
+                                Pattern r = Pattern.compile(patt);
+                                Matcher m = r.matcher(className);
+                                while (m.find()) {
+                                    XposedBridge.log("====之前===="+param.getResult());
+                                    Object obj=param.getResult();
+                                    Class userCla =  obj.getClass();
+                                    Field[] fs = userCla.getDeclaredFields();
+                                    for(int i = 0 ; i < fs.length; i++) {
+                                        Field f = fs[i];
+                                        f.setAccessible(true); //设置些属性是可以访问的
+                                        String type = f.getType().toString();//得到此属性的类型
+                                        if (type.endsWith("String")) {
+                                            f.set(obj, null);        //给属性设值
+                                        } else if (type.endsWith("int") || type.endsWith("Integer")) {
+                                            f.set(obj, 1);       //给属性设值
+                                        }
+                                    }
+                                    param.setResult(obj);
+                                    XposedBridge.log("====之后===="+param.getResult());
+                                }
+                            }catch (Exception e){
+                                e.printStackTrace();
+                            }
                         }
                     });
-                    XposedHelpers.findAndHookMethod(hookclass, "getCome", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            param.setResult(0);
-                            XposedBridge.log("====getCome===="+param.getResult());
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(hookclass, "getBsid", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                            param.setResult(null);
-                            XposedBridge.log("====getBsid===="+param.getResult());
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(hookclass, "getCopyright", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                            param.setResult(null);
-                            XposedBridge.log("====getCopyright===="+param.getResult());
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(hookclass, "getMoney", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            param.setResult("0");
-                            XposedBridge.log("====getMoney===="+param.getResult());
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(hookclass, "getOnline", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            param.setResult("1");
-                            XposedBridge.log("====getOnline===="+param.getResult());
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(hookclass, "getPrerequisite", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            param.setResult("0");
-                            XposedBridge.log("====getPrerequisite===="+param.getResult());
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(hookclass, "getPtid", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-//                            param.setResult(null);
-                            XposedBridge.log("====getPtid===="+param.getResult());
-                        }
-                    });
-                    XposedHelpers.findAndHookMethod(hookclass, "getPtname", new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            param.setResult(null);
-                            XposedBridge.log("====getPtname===="+param.getResult());
-                        }
-                    });*/
                 }catch (Exception e){
                     XposedBridge.log("寻找com.bixin.live.data.bean.room报错");
-                    return;
                 }
             }
         });
-    }
-
-
-    @Override
-    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
-
     }
 }
